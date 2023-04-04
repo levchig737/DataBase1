6d452105060b7ca3175625994322f6e79dedc550
levchig737 <levchekarev3@gmail.com>
2023-04-04 12:11:42 +0300

    add table alfa alfa

--- src/main/java/GUI/HelloApplication.java
+++ src/main/java/GUI/HelloApplication.java
@@ -7,12 +7,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Начальное окно TODO
+ */
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 400);
+        stage.setTitle("Доктора");
         stage.setScene(scene);
         stage.show();
     }
