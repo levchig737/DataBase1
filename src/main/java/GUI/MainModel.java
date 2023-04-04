fe43972b8afd86e8f1849001bdeb928723f83bfa
levchig737 <levchekarev3@gmail.com>
2023-04-04 14:42:50 +0300

    add buttons in HelloApplication, design some elements and min and max sizes table

--- src/main/java/GUI/MainModel.java
+++ src/main/java/GUI/MainModel.java
@@ -3,15 +3,12 @@ package GUI;
 import Connection.DBConnection;
 import TableEntity.Doctor;
 
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Основная модель с данным для таблицы
  */
 public class MainModel {
-//    private List<Doctor> doctorList = new ArrayList<>();
     private final DBConnection connection = new DBConnection();
 
 
